5ff53b437f74e88d202c3652743c1adbdb76d1d5
LuizFernando <barbosa.luiz@zup.com.br>
2025-01-07 09:58:48 -0300

    feat : Criando classe ReportSender

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ import exercicio1.GeradorRelatorioFianceiro;
 
 public class Main {
     public static void main(String[] args) {
-        
+
     }
 }
---
+++ src/exercicio1/ReportSender.java
@@ -0,0 +1,7 @@
+package exercicio1;
+
+public class ReportSender {
+    public void enviarPorEmail() {
+        System.out.println("Enviando relatorio por e-mail...");
+    }
+}
