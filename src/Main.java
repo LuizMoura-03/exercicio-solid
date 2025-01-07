598f3fce275b82d83b6bb4c262d61614684e91a8
LuizFernando <barbosa.luiz@zup.com.br>
2025-01-07 10:05:44 -0300

    feat : Adicionado geração e envio de relatório financeiro na Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 import exercicio1.GeradorRelatorioFianceiro;
+import exercicio1.ReportSender;
 
 public class Main {
     public static void main(String[] args) {
+        GeradorRelatorioFianceiro gerador = new GeradorRelatorioFianceiro();
+        ReportSender sender = new ReportSender() ;
+
+        gerador.gerarRelatorio();
+
+        sender.enviarPorEmail();
 
     }
 }
