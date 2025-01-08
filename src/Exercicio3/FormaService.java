353064584b798f7d07d16c2af337eeb6ce2f2a28
LuizFernando <barbosa.luiz@zup.com.br>
2025-01-08 15:12:14 -0300

    :sparkles: feat: Criando a classe FormaService para receber os dados de criação de formas geometricas que estão na Main

---
+++ src/Exercicio3/FormaService.java
@@ -0,0 +1,13 @@
+package Exercicio3;
+
+import java.util.Scanner;
+
+public class FormaService {
+    private final Scanner scanner;
+
+    public FormaService(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public Forma criarForma() {}
+}
--- src/Exercicio3/Quadrado.java
+++ src/Exercicio3/Quadrado.java
@@ -7,14 +7,6 @@ public class Quadrado implements Forma {
         this.lado = lado;
     }
 
-    public int getLado() {
-        return lado;
-    }
-
-    public void setLado(int lado) {
-        this.lado = lado;
-    }
-
     @Override
     public int getArea() {
         return lado * lado;
