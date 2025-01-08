0a79fbc1f6a48f76873637eebb6f5366f2c03850
LuizFernando <barbosa.luiz@zup.com.br>
2025-01-07 22:28:50 -0300

    feat: Criando a classe triangulo e adicionando calculo de area

---
+++ src/Exercicio3/Triangulo.java
@@ -0,0 +1,32 @@
+package Exercicio3;
+
+public class Triangulo implements Forma{
+    private int base;
+    public int altura;
+
+    public Triangulo(int base, int altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public int getBase() {
+        return base;
+    }
+
+    public void setBase(int base) {
+        this.base = base;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    @Override
+    public  int getArea() {
+        return (base * altura) / 2;
+    }
+}
