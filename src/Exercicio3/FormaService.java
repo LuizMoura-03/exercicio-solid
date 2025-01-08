31a9de634fe2a16c9f6c122a564b3ff4e6950008
LuizFernando <barbosa.luiz@zup.com.br>
2025-01-08 16:07:11 -0300

    :refactor: foi movido a logica de interação com usuario da Main para FormaService

--- src/Exercicio3/FormaService.java
+++ src/Exercicio3/FormaService.java
@@ -3,11 +3,63 @@ package Exercicio3;
 import java.util.Scanner;
 
 public class FormaService {
-    private final Scanner scanner;
+    private Scanner scanner;
 
-    public FormaService(Scanner scanner) {
-        this.scanner = scanner;
+    public FormaService() {
+        this.scanner = new Scanner(System.in);
     }
 
-    public Forma criarForma() {}
+    public void iniciar() {
+        boolean continuar = true;
+
+        while (continuar) {
+            System.out.println("===Bem vindo, Escolha uma forma Geometrica para calcular a área ===");
+            System.out.println("1. - Retangulo");
+            System.out.println("2. - Quadrado");
+            System.out.println("3. - Triangulo");
+            System.out.println("Digite o numero da forma: ");
+            int escola = scanner.nextInt();
+
+            Forma forma = null;
+
+            switch (escola) {
+                case 1:
+                    System.out.println("Digite a largura do retangulo: ");
+                    int largura = scanner.nextInt();
+                    System.out.print("Digite a altura do retângulo: ");
+                    int altura = scanner.nextInt();
+                    forma = new Retangulo(largura, altura);
+                    break;
+
+                case 2:
+                    System.out.print("Digite o lado do quadrado: ");
+                    int lado = scanner.nextInt();
+                    forma = new Quadrado(lado);
+                    break;
+
+                case 3:
+                    System.out.print("Digite a base do triângulo: ");
+                    int base = scanner.nextInt();
+                    System.out.print("Digite a altura do triângulo: ");
+                    int alturaTriangulo = scanner.nextInt();
+                    forma = new Triangulo(base, alturaTriangulo);
+                    continue;
+
+                default:
+                    System.out.println("Opção inválida! Tente novamente.");
+                    break;
+            }
+
+            if (forma != null) {
+                System.out.println("A área da forma é: " + forma.getArea());
+            }
+            System.out.print("Deseja calcular a área de outra forma? (s/n): ");
+            String resposta = scanner.next();
+            if (!resposta.equalsIgnoreCase("s")) {
+                continuar = false;
+            }
+        }
+        System.out.println("Obrigado por usar nosso sistema de cálculo de área!");
+        scanner.close();
+    }
 }
