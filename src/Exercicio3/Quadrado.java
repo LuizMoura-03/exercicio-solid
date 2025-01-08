fd2cce39d10f65b69503ee0e57991c18fb45089c
LuizFernando <barbosa.luiz@zup.com.br>
2025-01-07 21:55:23 -0300

    refactor: Refatorando a classe Retangulo para implementar a interface Forma

---
+++ src/Exercicio3/Quadrado.java
@@ -0,0 +1,16 @@
+package Exercicio3;
+
+public class Quadrado extends Retangulo {
+    @Override
+    public void setLargura(int largura) {
+        this.largura = largura;
+        this.altura = largura;
+    }
+
+    @Override
+    public void setAltura(int altura) {
+        this.altura = altura;
+        this.largura = altura;
+    }
+
+}
--- src/Exercicio3/Retangulo.java
+++ src/Exercicio3/Retangulo.java
@@ -1,33 +1,28 @@
 package Exercicio3;
 
-public class Retangulo {
+public class Retangulo implements Forma{
     protected int largura;
     protected int altura;
 
+    public void Retangulo(int largura, int altura) {
+        this.largura = largura;
+        this.altura = altura;
+
+    } public int getLargura() {
+        return largura;
+    }
+
     public void setLargura(int largura) {
         this.largura = largura;
+    }
+
+    public int getAltura() {
+        return altura;
     }
 
     public void setAltura(int altura) {
         this.altura = altura;
     }
 
-    public int getArea() {
-        return largura * altura;
-    }
 }
 
-public class Quadrado extends Retangulo {
-    @Override
-    public void setLargura(int largura) {
-        this.largura = largura;
-        this.altura = largura;
-    }
-
-    @Override
-    public void setAltura(int altura) {
-        this.altura = altura;
-        this.largura = altura;
-    }
-
-}
