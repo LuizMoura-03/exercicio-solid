7a79f9de08014aff9ed2a4d34a39394b596c7072
LuizFernando <barbosa.luiz@zup.com.br>
2025-01-09 11:09:03 -0300

    feat: Criando o exercicio5 com suas classes'Teclado e Computador' para serem refatoradas

---
+++ src/exercicio5/Computador.java
@@ -0,0 +1,10 @@
+package exercicio5;
+//Código inicial (errado):
+
+public class Computador {
+    private Teclado teclado;
+
+    public Computador() {
+        this.teclado = new Teclado();
+    }
+}
---
+++ src/exercicio5/Teclado.java
@@ -0,0 +1,8 @@
+package exercicio5;
+//Código inicial (errado):
+
+public class Teclado {
+    public void digitar() {
+        System.out.println("Digitando...");
+    }
+}
